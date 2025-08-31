54cae10ff33bd8981fb8ed0dcbd185c2ef336a9e
Sayeed Munees <sayeedmunees0@gmail.com>
2025-08-31 20:31:56 +0530

    Added styles

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
+// const express = require('express');
+// const { createServer } = require('node:http');
+// const { join } = require('node:path');
+// const { Server } = require('socket.io');
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(join(__dirname, 'index.html'));
+// });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
+// io.on('connection', (socket) => {
+//   socket.on('chat message', (msg) => {
+//     io.emit('chat message', msg);
+//   });
+// });
